b77d2b952f2479b5ca05d358b01710e349ec6844
AlenBiz <jobbizyaev@gmail.com>
2023-01-30 23:44:42 +0300

    added a cucumber

--- cypress.config.js
+++ cypress.config.js
@@ -1,4 +1,9 @@
 const { defineConfig } = require("cypress");
+const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
+const addCucumberPreprocessorPlugin =
+require("@badeball/cypress-cucumber-preprocessor").addCucumberPreprocessorPlugin;
+const createEsbuildPlugin =
+require("@badeball/cypress-cucumber-preprocessor/esbuild").createEsbuildPlugin;
 
 module.exports = defineConfig({
   defaultCommandTimeout: 8000, 
@@ -6,11 +11,17 @@ module.exports = defineConfig({
   e2e: {
     baseUrl: "https://staging.lpitko.ru",
     testIsolation: false,
-    
+    specPattern: "**/*.feature",
     setupNodeEvents(on, config) {
+      
+      const bundler = createBundler({
+      plugins: [createEsbuildPlugin(config)],
+      });
+      
+      on("file:preprocessor", bundler);
+      addCucumberPreprocessorPlugin(on, config);
+      return config;
   
-    
-      // implement node event listeners here
     },
   },
 });
---
+++ cypress/e2e/testBDD/login.feature
@@ -0,0 +1,5 @@
+Feature: User login on santa website
+Scenario: User logs in sucessfulls 
+Given: user on secret santa login page
+When: user logs in
+Then: user is dashboard page
